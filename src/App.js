88073cc5ced1e2db9b21200919117e2afcb4e49d
Pascal Zarzar <pascalzarzar@MacBook-Air-de-pascal.local>
2021-12-20 20:00:12 -0300

    React router installed and app routes ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+          <Route exact path='/' element={<h1>Feed</h1>}/>
+          <Route exact path='/profile' element={<h1>profile</h1>}/>
+          <Route exact path='/login' element={<h1>login</h1>}/>
+          <Route exact path='/register' element={<h1>register</h1>}/>
+      </Routes>
     </div>
+    
   );
 }
 
