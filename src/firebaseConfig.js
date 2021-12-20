f5bf3a529a6a5865b0648c76ca0ad44a25aea3b5
Pascal Zarzar <pascalzarzar@MacBook-Air-de-pascal.local>
2021-12-20 19:47:55 -0300

    firebaseConfig file ready with firestore, storage and auth installed

---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+import 'firebase/storage'
+import 'firebase/auth'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCalDwo0weYn1l8GSc2WzRMdhnEi7PVATA",
+    authDomain: "devs-united-834e1.firebaseapp.com",
+    projectId: "devs-united-834e1",
+    storageBucket: "devs-united-834e1.appspot.com",
+    messagingSenderId: "1000155230616",
+    appId: "1:1000155230616:web:241d812d00f6005ad73bed"
+};
+
+//initialize firebase
+firebase.initializeApp(firebaseConfig);
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
+export const auth = firebase.auth();
+export default firebase;
